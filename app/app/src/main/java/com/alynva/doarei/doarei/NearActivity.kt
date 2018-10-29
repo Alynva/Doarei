3889ff38fa7c2bb2e4113074b693192163ca6d56
Alisson Nunes <alynva@gmail.com>
2018-10-29 10:11:15 -0300

    Teste (porco) do Firebase Functions

--- app/app/src/main/java/com/alynva/doarei/doarei/NearActivity.kt
+++ app/app/src/main/java/com/alynva/doarei/doarei/NearActivity.kt
@@ -5,15 +5,32 @@ import android.os.Bundle
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
+import com.google.firebase.functions.FirebaseFunctions
 import kotlinx.android.synthetic.main.activity_near.*
+import com.google.android.gms.tasks.Continuation
+import com.google.firebase.functions.HttpsCallableResult
+import com.google.android.gms.tasks.Task
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class NearActivity : AppCompatActivity() {
+
+    companion object {
+        val mFunctions = FirebaseFunctions.getInstance();
+    }
 
     var nearList : MutableList<NearEntity> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_near)
+
+        btnCampanha.setOnClickListener {
+            addMessage("Qualquercoisa").addOnCompleteListener {
+                task: Task<String> -> Toast.makeText(this, task.result, Toast.LENGTH_SHORT).show()
+            }
+        }
 
         carregaLista()
     }
@@ -32,5 +49,26 @@ class NearActivity : AppCompatActivity() {
         rvNear.adapter = adapter
         rvNear.layoutManager = layoutManager
         rvNear.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun addMessage(text: String): Task<String> {
+        // Create the arguments to the callable function.
+        val data = HashMap<String, Any>().apply {
+            put("text", text)
+            put("push", true)
+        }
+
+        return mFunctions
+                .getHttpsCallable("helloWorld")
+                .call(data)
+                .continueWith(object : Continuation<HttpsCallableResult, String> {
+                    @Throws(Exception::class)
+                    override fun then(task: Task<HttpsCallableResult>): String {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        return task.result.data as String
+                    }
+                })
     }
 }
